92c496377b0f54615b8fe553bb9175c8352e5fa6
Andy Whitfield <andy@andywhitfield.me.uk>
2016-04-13 11:33:51 +0100

    Initial SmallId skeleton, with some openauth in there (not yet working).

---
+++ SmallId/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmallId.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Response.AppendHeader(
+                "X-XRDS-Location",
+                new Uri(Request.Url, Response.ApplyAppPathModifier("~/Home/xrds")).AbsoluteUri);
+            return View();
+        }
+
+        public ActionResult Xrds()
+        {
+            return View("Xrds");
+        }
+    }
+}
