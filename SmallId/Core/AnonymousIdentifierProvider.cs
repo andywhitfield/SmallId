af28753e5d76d8f646082ea06767fef763fc9f3f
Andy Whitfield <andy@andywhitfield.me.uk>
2016-04-19 16:21:52 +0100

    As it is before I overwrite with the MVC pre-canned sample.

--- SmallId/Core/AnonymousIdentifierProvider.cs
+++ SmallId/Core/AnonymousIdentifierProvider.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using DotNetOpenAuth.OpenId;
+using DotNetOpenAuth.OpenId.Provider;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 
 namespace SmallId.Core
 {
     public class AnonymousIdentifierProvider : PrivatePersonalIdentifierProviderBase
     {
+        internal AnonymousIdentifierProvider() : base("anon?id=".GetAppPathRootedUri())
+        {
+        }
+
+        protected override byte[] GetHashSaltForLocalIdentifier(Identifier localIdentifier)
+        {
+            var membership = (ReadOnlyXmlMembershipProvider)Membership.Provider;
+            string username = User.GetUserFromClaimedIdentifier(new Uri(localIdentifier));
+            return Convert.FromBase64String(membership.GetSalt(username));
+        }
     }
 }
